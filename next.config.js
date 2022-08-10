82553d7e330201c189912e401746569925f58643
FdR-23 <federampi@gmail.com>
2022-08-10 01:03:19 -0300

    fixed_env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_KEY: 'e7bf97cc7349fa6877408e171616f985',
+  },
 }
 
-module.exports = nextConfig
+
+
+
+
+module.exports =  nextConfig
